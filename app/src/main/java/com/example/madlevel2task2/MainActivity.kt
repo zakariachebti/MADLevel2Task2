8932909ec674113cc222404335cb33acf8e1e285
Zakaria Chebti <zakaria@chipta.com>
2020-10-16 10:38:50 +0200

    Klaar

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +25,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews(){
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
 
+        for (i in Question.QUESTION.indices) {
+            questions.add(Question(Question.QUESTION[i], Question.ANSWER[i] ))
+        }
+
+        createItemTouchHelper().attachToRecyclerView(binding.rvQuestions)
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                if ((direction === ItemTouchHelper.LEFT && questions[position].answer === false) ||
+                    (direction === ItemTouchHelper.RIGHT && questions[position].answer === true)) {
+                    questions.removeAt(position)
+                } else {
+                    Snackbar.make(viewHolder.itemView, "The Question will not be removed from the list.", Snackbar.LENGTH_SHORT).show()
+                }
+
+
+
+                questionAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 }
